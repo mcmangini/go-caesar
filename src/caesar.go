dd88a967f8c214c2e19abe2e16fa2d900cbe7c70
mcmangini <mark.c.mangini@gmail.com>
2025-10-14 00:26:29 -0400

    Implement Caesar cipher algorithm

--- src/caesar.go
+++ src/caesar.go
@@ -5,7 +5,36 @@ import (
 	"os"
 )
 
+func caesarCipher(shift rune, in string) string {
+	shift %= 26
+	if shift < 0 {
+		shift = 26 + shift
+	}
+	ucStart := 'A'
+	ucEnd := 'Z'
+	lcStart := 'a'
+	lcEnd := 'z'
+	out := ""
+	for _, c := range in {
+		var start rune
+		if c >= ucStart && c <= ucEnd {
+			// A - Z
+			start = ucStart
+		} else if c >= lcStart && c <= lcEnd {
+			// a - z
+			start = lcStart
+		} else {
+			out += string(c)
+			continue
+		}
+		out += string((c - start + shift) % 26 + start)
+	}
+	return out
+}
+
 func main() {
 	args := os.Args[1:]
 	fmt.Println(args)
+	out := caesarCipher(-72, "This is a test")
+	fmt.Println(out)
 }
